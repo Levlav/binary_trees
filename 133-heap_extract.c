7e816777451be722dbb1dd64727fe2f28ed4dee5
Levlav <levinlavatsa.com>
2023-08-31 10:49:20 -0700

    binary_trees

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * heap_extract - extract node
- * @root: root of the tre to evaluate
- * Return: value
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @root: double pointer to the root node of heap
+ * Return: value stored in the root node
  */
-int heap_extract(heap_t **root)
+int heap_extract(heap_t **root);
 {
-	return (0);
+	return (root);
 }
